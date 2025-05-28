a6a01b9677ccd0debafda8a87b2cc882ba96a136
MalachiM47 <mmadu0005@mymail.lausd.net>
2025-05-28 10:35:58 -0700

    Update Test.java

---
+++ src/main/java/com/MBfinchCSA/Test.java
@@ -0,0 +1,16 @@
+package com.MBfinchCSA;
+
+public class Test {
+
+    public static void main(String[]args){
+        System.out.println("Hello:");
+
+        int[] score = new int[20];
+
+        for(int i : score){
+            System.out.println(i);
+        }
+
+
+    }
+}
